a3aa7b06b8597baf4f671101c8252c426a9940c9
inggilove <inggilove@gmail.com>
2018-02-22 00:49:58 +0900

    Hystrix Dashboard added. but Eureka error message is shown..

--- src/main/java/com/example/demo/DemoApigwAdminApplication.java
+++ src/main/java/com/example/demo/DemoApigwAdminApplication.java
@@ -3,14 +3,27 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@EnableHystrixDashboard
+//@EnableTurbine
 @SpringBootApplication
 //@EnableAutoConfiguration
-@EnableAdminServer
+//@EnableAdminServer
+//@RestController
 public class DemoApigwAdminApplication {
 
+//    @RequestMapping("/")
+//    public String home() {
+//        return "forward:/hystrix";
+//    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApigwAdminApplication.class, args);
 	}
